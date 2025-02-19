c55bd856dbaeef7d0ee31b8974eca082ac950d77
Apostle01 <148454484+Apostle01@users.noreply.github.com>
2025-02-19 18:26:36 +0000

    deleted:    shop/templates/shop/shop.html         modified:   shop/templates/shop/shop_now.html         modified:   shop/urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -3,12 +3,13 @@ from . import views
 from .views import custom_login
 
 urlpatterns = [
+    path('', views.shop_now, name='shop_now'),  # This sets '/' as the homepage
+    path('shop_now/', views.shop_now, name='shop_now'),  # Add this if needed
     path('', views.index, name='index'),
     path('category/<slug:slug>/', views.category, name='category'),
     path('about/', views.about, name='about'),  
     path('contact/', views.contact, name='contact'),  
-    # path('shop/', views.shop, name='shop'),
-    path('', views.shop_now, name='shop_now'), # set as home page
+    
     
     # Cart & Checkout URLs
     path('add-to-cart/', views.add_to_cart, name='add_to_cart'),
