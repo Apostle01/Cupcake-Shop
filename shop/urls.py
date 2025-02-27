2a4ad7b274615ff9924e61121383336c5691fef3
Apostle01 <148454484+Apostle01@users.noreply.github.com>
2025-02-27 20:34:17 +0000

    modified shop/urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -4,20 +4,19 @@ from .views import custom_login
 
 urlpatterns = [
     path('', views.home, name='home'),  # Ensuring correct view function is called
-    path('shop/', views.shop_now, name='shop'),  # Add this if needed
+    path('shop/', views.shop, name='shop'),
     path('category/<slug:slug>/', views.category, name='category'),
     path('about/', views.about, name='about'),  
     path('contact/', views.contact, name='contact'), 
     
     
     # Cart & Checkout URLs
-    path('add-to-cart/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.view_cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('submit_order/', views.submit_order, name='submit_order'),
     path('payment_options/', views.payment_options, name='payment_options'),
     path('login/', custom_login, name='login'),
     path('add_review', views.add_review, name='add_review'),
-    path('add-to-cart/<int:cupcake_id>/', views.add_to_cart, name='add_to_cart'),  
+    path('add_to_cart/<int:cupcake_id>/', views.add_to_cart, name='add_to_cart'),  
 ]
 
