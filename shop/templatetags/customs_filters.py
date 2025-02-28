94e941c858e487d281a758c138b1c895e1ff0785
Apostle01 <148454484+Apostle01@users.noreply.github.com>
2025-02-28 19:55:11 +0000

    modified:   shop/templates/shop/cart.html         modified:   shop/templatetags/customs_filters.py

--- shop/templatetags/customs_filters.py
+++ shop/templatetags/customs_filters.py
@@ -1,8 +1,10 @@
 # templatetags/custom_filters.py
+print("Loading custom_filters library...")  # Debugging statement
 from django import template
 
 register = template.Library()
 
 @register.filter
 def multiply(value, arg):
+    """Multiple the value by the argument"""
     return value * arg
