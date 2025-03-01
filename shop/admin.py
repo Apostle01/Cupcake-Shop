46bac71703afb7da321ee30b14d3d6701bde7c84
Apostle01 <148454484+Apostle01@users.noreply.github.com>
2025-03-01 00:59:45 +0000

    modified:   db.sqlite3         modified:   shop/admin.py and added shop/templates/shop/order_detail.html template

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Category, Cupcake, Order
+from .models import Category, Product, Cupcake, Cart, CartItem, Order, OrderItem, Review
 
 admin.site.register(Category)
+admin.site.register(Product)
 admin.site.register(Cupcake)
+admin.site.register(Cart)
+admin.site.register(CartItem)
 admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Review)
 
